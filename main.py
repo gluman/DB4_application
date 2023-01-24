ef56781bc0795d22f115b308f44e888b002daf4e
Glumov Andrey <gluman@yandex.ru>
2023-01-24 19:05:07 +0300

    commit 24.01.2023 add some function

--- main.py
+++ main.py
@@ -20,11 +20,39 @@ import pprint as pp
 
 run = True
 while run:
-    with psycopg2.connect(database="clientdatabase", user="postgres", password="postgres") as conn:
+    with psycopg2.connect(database="clientdatabase", user="postgres", password="postgres", host='localhost') as conn:
         with conn.cursor() as cur:
+            def check_exist_client(name, surname, email):
+                '''Check exist client in database'''
+                # try:
+                cur.execute('''SELECT * FROM clients c
+                WHERE (c.name = %s AND c.surname = $s) OR email= %s;
+                ''', (name, surname, email))
+                conn.commit()
+                result = cur.fetchone()[0]
+                # except:
+                #     return 'miss_data'  # if not rows in table
+                # else:
+                if result != 0:
+                    return False    # Check is NOT OK
+                else:
+                    return True     # Check is OK
 
-            def check_exist_client():
-                pass
+            def check_exist_phonenum(number):
+                '''Check exist phone in database'''
+                try:
+                    cur.execute('''
+                    SELECT count(*) FROM phone_numbers pn
+                    WHERE pn.number = %s;
+                    ''', (number,))
+                    result = cur.fetchone()
+                except:
+                    return 'miss_data'
+                else:
+                    if result != 0:
+                        return False    # Check is NOT OK
+                    else:
+                        return True     # Check is OK
 
             def drop_data():
                 # удаление таблиц
@@ -32,35 +60,38 @@ while run:
                 DROP TABLE phone_numbers;
                 DROP TABLE clients;
                 """)
+                conn.commit()
 
             def show_help():
                 print('''
                     c - create tables
                     h - show help
                     a - add client
+                    ap - add clients phone number 
                     l - list clients
                     e - edit clients information
                     d - delete clients
+                    drop - drop tables
                     f - find clients
                     x - exit
                     ''')
 
             def create_tables():
                 cur.execute("""
-                         CREATE TABLE IF NOT EXISTS phone_numbers(
-                             id_pn SERIAL PRIMARY KEY,
-                             number VARCHAR(40) UNIQUE,
-                             id_cl INTEGER NOT NULL REFERENCES clients(id)
-                         );
-                         """)
-                cur.execute("""
                         CREATE TABLE IF NOT EXISTS clients(
                             id_cl SERIAL PRIMARY KEY,
                             name TEXT NOT NULL,
                             surname TEXT NOT NULL,
-                            email VARCHAR(30) UNIQUE,
-                            id_pn INTEGER REFERENCES phone_numbers(id_pn)
-                                );
+                            email VARCHAR(30) UNIQUE
+                            );
+                         """)
+
+                cur.execute("""
+                         CREATE TABLE IF NOT EXISTS phone_numbers(
+                             id_pn SERIAL PRIMARY KEY,
+                             number VARCHAR(40) UNIQUE,
+                             id_cl INTEGER NOT NULL REFERENCES clients(id_cl)
+                         );
                          """)
                 conn.commit()  # фиксируем в БД
 
@@ -70,16 +101,38 @@ while run:
                 email = input('Please, input clients email: ')
                 phone = input('Please, input clients phone number(Enter to skip): ')
 
-                cur.execute("""
-                    INSERT INTO client(name, surname,) VALUES('Java') RETURNING id;
-                    """)
-                pp(cur.fetchone())  # запрос данных автоматически зафиксирует изменения
+                check = check_exist_client(name, surname, email)
+                if check != False:
+                    cur.execute("""
+                        INSERT INTO clients(name, surname, email) VALUES(%s, %s, %s) RETURNING id_cl;
+                        """, (name, surname, email))
+                    id_cl = cur.fetchone()  # запрос данных автоматически зафиксирует изменения   --- почему то не добавляет запись в бд, только возварщает id
+                    conn.commit()
+                    add_phone_number(phone, id_cl)
+                else:
+                    pp('Error! Some wrong with data')
+
+            def add_phone_number(phone, id_cl):
+                if phone != '':
+                    check_phone = check_exist_phonenum(phone)
+                    if check_phone != False:
+                        cur.execute("""
+                            INSERT INTO phone_numbers(number, id_cl) VALUES(%s, %s) RETURNING id_pn;
+                            """, (phone, id_cl))
+                        pp('''Client added:''', cur.fetchone())
+                        conn.commit()
+                        # запрос данных автоматически зафиксирует изменения
+                    else:
+                        pp('This phone alredy exist')
 
             def list_clients():
-                cur.execute("""
-                         SELECT  FROM clients;
-                         """)
-                pp('fetchall', cur.fetchall())  # извлечь все строки
+                if check_exist_client() != 'miss_data':
+                    cur.execute("""
+                             SELECT * FROM clients;
+                             """)
+                    pp('fetchall', cur.fetchall())  # извлечь все строки
+                else:
+                    pp('No information in database')
 
             def edit_client():
                 pass
@@ -92,26 +145,33 @@ while run:
 
 
 
-    i = input('input command, h - help')
-    if i == 'c':
-        create_tables()
-    if i == 'h':
-        show_help()
-    if i == 'a':
-        add_client()
-    if i == 'l':
-        list_clients()
-    if i == 'e':
-        edit_client()
-    if i == 'd':
-        delete_client()
-    if i == 'drop':
-        drop_data()
-    if i == 'f':
-        find_clinet()
-    if i == 'x':
-        run = False
-    conn.close()
+            i = input('input command, h - help: ')
+            if i == 'c':
+                create_tables()
+            elif i == 'h':
+                show_help()
+            elif i == 'a':
+                add_client()
+            elif i == 'ap':
+                add_phone_number()
+            elif i == 'l':
+                list_clients()
+            elif i == 'e':
+                edit_client()
+            elif i == 'd':
+                delete_client()
+            elif i == 'drop':
+                drop_data()
+            elif i == 'f':
+                find_clinet()
+            elif i == 'x':
+                run = False
+                # else:
+                #     pp('unknow command. h - help')
+                #     continue
+
+
+    # conn.close()
 
     # class Client:
     # def __int__(self, name, surname, email, phone):
